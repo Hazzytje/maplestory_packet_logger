c572dffa82d0d561e50d736d34e21bfc8cf8b6fd
Harry van Wier <harryvanwier@gmail.com>
2017-10-28 21:10:11 +0200

    Refactor original bytes and add string and buffer

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <windows.h>
 
 const unsigned char jmp_opcode = 0xE9;
+const unsigned char nop_opcode = 0x90;
 
 const int send_packet_addr = 0x0049637B;
 const int encode1_addr = 0x00406549;
@@ -52,13 +53,16 @@ void open_console() {
 	printf("Debugging Window:\n");
 }
 
-void hook(int addr, void* hook) {
+void hook(int addr, void* hook, int orig_padding = 0) {
 	printf("hooking: %08X pointed to %08X\n", addr, reinterpret_cast<int>(hook) - addr - 5);
 	char bla = char(1000);
 	char vla = static_cast<char>(1000);
 
 	*((unsigned char*)addr) = jmp_opcode;
 	*((int*)(addr + 1)) = reinterpret_cast<int>(hook) - addr - 5;
+	for (int i = 0; i < orig_padding; i++) {
+		*(char*)(addr + 5 + i) = nop_opcode;
+	}
 }
 
 bool launchDebugger() {
@@ -95,28 +99,23 @@ bool launchDebugger() {
 	return true;
 }
 
-void* make_hook_page(int hook_addr, int ret_addr, unsigned char writelen) {
-	unsigned char shellcode[] = {
-		0x60, //pushad
-		0xE8, 0x00, 0x00, 0x00, 0x00, //call
-		0x61, //popad
-		0x56, //push esi
-		0x8B, 0xF1, //mov esi, ecx
-		0x6A, writelen, //push 1
-		0x68, 0x00, 0x00, 0x00, 0x00, //push 
-		0xC3 //ret
-	};
-	printf("allocating memory...\n");
-	unsigned char* shellcode_page = static_cast<unsigned char*>(VirtualAlloc(nullptr, 1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE));
-	printf("allocated memory, at %p, now copying shellcode into buffer\n", shellcode_page);
-	memcpy(shellcode_page, shellcode, sizeof(shellcode));
+void* make_hook_page(int hook_addr, int ret_addr, unsigned char* orig_bytes, int orig_byte_size) {
+	unsigned char* shellcode = static_cast<unsigned char*>(VirtualAlloc(nullptr, 1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE));
+	printf("allocated memory at %p, now copying shellcode into buffer\n", shellcode);
 
-	int relative_hook_addr = hook_addr - reinterpret_cast<int>(shellcode_page) - 6;
-	memcpy(shellcode_page + 2, &relative_hook_addr, 4); //copy hook addr over 0x00 bytes
-	memcpy(shellcode_page + 13, &ret_addr, 4); //copy ret addr over 0x00 bytes
+	int relative_hook_addr = hook_addr - reinterpret_cast<int>(shellcode) - 6;
 
-	printf("copied the addresses into the shellcode\n");
-	return shellcode_page;
+	int i = 0;
+	shellcode[i++] = 0x60; //pushad
+	shellcode[i++] = 0xE8; //call
+	*reinterpret_cast<int*>(shellcode + i) = relative_hook_addr; i += 4; //call addr
+	shellcode[i++] = 0x61; //popad
+	memcpy(shellcode + i, orig_bytes, orig_byte_size); i += orig_byte_size; //original bytes
+	shellcode[i++] = 0x68; //push
+	*reinterpret_cast<int*>(shellcode + i) = ret_addr; i += 4; //ret addr
+	shellcode[i++] = 0xC3; //ret
+	
+	return shellcode;
 }
 
 BOOL WINAPI DllMain(_In_ HINSTANCE hinstDLL, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved) {
@@ -144,9 +143,21 @@ BOOL WINAPI DllMain(_In_ HINSTANCE hinstDLL, _In_ DWORD fdwReason, _In_ LPVOID l
 	printf("function encodebuf is at %p\n", (void*)encode_buffer_dll_addr);
 	printf("function sent is at %p\n", (void*)packet_sent_dll_addr);
 
-	hook(encode1_addr, make_hook_page(encode1_dll_addr, encode1_addr + 5, 1));
-	hook(encode2_addr, make_hook_page(encode2_dll_addr, encode2_addr + 5, 2));
-	hook(encode4_addr, make_hook_page(encode4_dll_addr, encode4_addr + 5, 4));
+	unsigned char encode_orig[] = {0x56, 0x8B, 0xF1, 0x6A, 0x01};
+	hook(encode1_addr, make_hook_page(encode1_dll_addr, encode1_addr + 5, encode_orig, sizeof(encode_orig)));
+	encode_orig[4] = 2;
+	hook(encode2_addr, make_hook_page(encode2_dll_addr, encode2_addr + 5, encode_orig, sizeof(encode_orig)));
+	encode_orig[4] = 4;
+	hook(encode4_addr, make_hook_page(encode4_dll_addr, encode4_addr + 5, encode_orig, sizeof(encode_orig)));
+
+	unsigned char buffer_orig[] = {0x56, 0x57, 0x8B, 0x7C, 0x24, 0x10};
+	hook(encode_buffer_addr, make_hook_page(encode_buffer_dll_addr, encode_buffer_addr + 5, buffer_orig, sizeof(buffer_orig)), 1);
+
+	unsigned char str_orig[] = {0xB8, 0x00, 0xE5, 0xA7, 0x00};
+	hook(encode_str_addr, make_hook_page(encode_string_dll_addr, encode_str_addr + 5, str_orig, sizeof(str_orig)));
+
+	unsigned char send_orig[] = {0xB8, 0x6C, 0x12, 0xA8, 0x00};
+	hook(send_packet_addr, make_hook_page(packet_sent_dll_addr, send_packet_addr + 5, send_orig, sizeof(send_orig)));
 
 	return TRUE;
 }
